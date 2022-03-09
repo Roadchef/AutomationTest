ac8e85175db2383268320b4afac03349b86c60d2
ben.latham@roadchef.com <ben.latham@roadchef.com>
2022-03-09 10:24:58 +0000

    Add main.yml

---
+++ .github/workflows/scripts/runChecks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+space=$1
+project=$2
+
+result=$(curl -s 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/Roadchef_dev/Sandbox_BL/GithubManagement/RunQualityChecks%20Task?repo='"${repo}" -H "Authorization: Bearer oviByeqdn2G0gtQzkBPuSr8h3y9Gl8Mp")
+success=`echo ${result} | jq -r '.[].success'`
+text=`echo ${result} | jq -r '.[].text'`
+
+if [[ $success == "true" ]]; then
+    echo "success: ${success}"
+    echo "Tests completed successfully"
+    echo "$text"
+    exit 0
+else
+    echo "Tests failed"
+    echo "$text"
+    exit -1
+fi
