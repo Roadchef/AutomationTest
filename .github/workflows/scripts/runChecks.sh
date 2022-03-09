8f209a8b677bbedc9b267f1805df44e91103fae5
blatham191 <39377538+blatham191@users.noreply.github.com>
2022-03-09 10:47:21 +0000

    Update runChecks.sh

--- .github/workflows/scripts/runChecks.sh
+++ .github/workflows/scripts/runChecks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-space=$1
-project=$2
+repo=$1
 
 result=$(curl -s 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/Roadchef_dev/Sandbox_BL/GithubManagement/RunQualityChecks%20Task?repo='"${repo}" -H "Authorization: Bearer oviByeqdn2G0gtQzkBPuSr8h3y9Gl8Mp")
 success=`echo ${result} | jq -r '.[].success'`
